7cc49eac36c043c0abc25289d0ae99ac5f0c13a2
Nils Andresen <nils@nils-andresen.de>
2020-10-08 22:07:19 +0200

    fixed build on gh-actions

--- src/Quartz.LightCore.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("502cb80f-d900-4a40-9a3e-48f0cf67e9e9")]
