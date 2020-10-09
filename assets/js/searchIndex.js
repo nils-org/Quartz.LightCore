0a9da9ae5401ca652e1bd8d53087bdfdefcd120b
Nils Andresen <nils@nils-andresen.de>
2020-10-09 05:21:46 +0000

    Continuous Integration Publish: 97dfd915a0017c83631720f8d7c153f10da79525 Merge branch 'main' into develop

---
+++ assets/js/searchIndex.js
@@ -0,0 +1,73 @@
+
+var camelCaseTokenizer = function (builder) {
+
+  var pipelineFunction = function (token) {
+    var previous = '';
+    // split camelCaseString to on each word and combined words
+    // e.g. camelCaseTokenizer -> ['camel', 'case', 'camelcase', 'tokenizer', 'camelcasetokenizer']
+    var tokenStrings = token.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
+      var current = cur.toLowerCase();
+      if (acc.length === 0) {
+        previous = current;
+        return acc.concat(current);
+      }
+      previous = previous.concat(current);
+      return acc.concat([current, previous]);
+    }, []);
+
+    // return token for each string
+    // will copy any metadata on input token
+    return tokenStrings.map(function(tokenString) {
+      return token.clone(function(str) {
+        return tokenString;
+      })
+    });
+  }
+
+  lunr.Pipeline.registerFunction(pipelineFunction, 'camelCaseTokenizer')
+
+  builder.pipeline.before(lunr.stemmer, pipelineFunction)
+}
+var searchModule = function() {
+    var documents = [];
+    var idMap = [];
+    function a(a,b) { 
+        documents.push(a);
+        idMap.push(b); 
+    }
+
+    a(
+        {
+            id:0,
+            title:"SchedulerExtension",
+            content:"SchedulerExtension",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Quartz.LightCore/api/Quartz.LightCore/SchedulerExtension',
+            title:"SchedulerExtension",
+            description:""
+        }
+    );
+    var idx = lunr(function() {
+        this.field('title');
+        this.field('content');
+        this.field('description');
+        this.field('tags');
+        this.ref('id');
+        this.use(camelCaseTokenizer);
+
+        this.pipeline.remove(lunr.stopWordFilter);
+        this.pipeline.remove(lunr.stemmer);
+        documents.forEach(function (doc) { this.add(doc) }, this)
+    });
+
+    return {
+        search: function(q) {
+            return idx.search(q).map(function(i) {
+                return idMap[i.ref];
+            });
+        }
+    };
+}();
